76752d6baca49e961191a7bc9bd565d36925af03
guilherme  henrique boing <guilhermeboing@hotmail.com>
2022-05-18 17:12:54 -0300

    sim

--- scripts.js
+++ scripts.js
@@ -1,16 +1,76 @@
-new Chart("myChart", {
-    type: "pie",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
+const data = {
+  labels: ["\uf13b HTML", "\uf3b8 Javascript ", "\uf420 Angular", "\uf38b CSS"],
+  datasets: [
+    {
+      label: "tempo utilizando a tecnologia (anos)",
+      backgroundColor: "#0B13D1",
+      borderColor: "black",
+      borderWidth: "1",
+      borderSkipped: false,
+      data: [7.5, 5.3, 0.8, 7]
     },
-    options: {
-      title: {
-        display: true,
-        text: "World Wide Wine Production"
+    {
+      label: "Total de Anos trabalhando com a tecnologia",
+      backgroundColor: "#8E00FD",
+      borderColor: "black",
+      borderWidth: "1",
+      borderSkipped: false,
+      data: [2, 2, 2, 2]
+    },
+    {
+      label: "Cursos efetuados sobre a tecnologia",
+      backgroundColor: "#F2AE00",
+      borderColor: "black",
+      borderWidth: "1",
+      borderSkipped: false,
+      data: [2, 3, 0, 2]
+    }
+  ]
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    grouped: true,
+    scales: {
+      xAxes: [{
+        gridLines:{
+          display:false
+        },
+        ticks: {
+          fontFamily: 'FontAwesome'
+        }
+      }],
+      yAxes: [{
+        gridLines:{
+          display:false
+        },
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  },
+  plugins: {
+    legend: {
+      labels: {
+        font: {
+          size: 35
+        }
       }
     }
-  });
+  }
+
+
+};
+
+// render init block
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+
+
+Chart.defaults.global.defaultFontColor = "black";
